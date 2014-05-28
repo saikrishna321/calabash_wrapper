e31398eafb243b76a3d3197c304009cc64892d70
saikrishna <saikrishna321@yahoo.com>
2014-05-28 10:43:57 +0530

    Added screenshot embed with respective method name

--- commonmethods/app_wrappers.rb
+++ commonmethods/app_wrappers.rb
@@ -19,11 +19,18 @@ class AppWrapper < Calabash::ABase
 	end
 
 
+
 	def shop_all_groceries
+
+		wait_for_element($shop_all_groceries_dashboard, 10)
 		touch_screen($shop_all_groceries_dashboard)
+		wait_for_element('Fresh Food', 10)
 		touch_screen('Fresh Food')
+		wait_for_element('Fresh Fruit', 10)
 		touch_screen('Fresh Fruit')
+	  screenshot_embed(:name=> "#{__method__}.png")
 		touch_screen('Bananas')
+
 	end
 
 
@@ -35,11 +42,14 @@ class AppWrapper < Calabash::ABase
 
 # This method helps the user to singin with respective credentials
 	def login(username, password)
-
+		sleep(2)
 		touch_screen($sign_in)
+		wait_for_element($user_name,5)
 		send_text($user_name, username)
 		send_text($password, password)
 		touch_screen($sign_in)
+
+
 	end
 
 
