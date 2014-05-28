b9d0ffe7499bd2bbddc8c5842d2ca392089b14cc
saikrishna <saikrishna321@yahoo.com>
2014-05-28 09:22:06 +0530

    Added appId's

--- commonmethods/appIds.rb
+++ commonmethods/appIds.rb
@@ -2,9 +2,10 @@
 module Testdata
 	$sign_in='signin_btn'
 	$user_name='username_et'
+	$password='password_et'
   $test='test'
 	$side_nav='action_bar_up_navigation'
-
+  $shop_all_groceries_dashboard='anonymous_shop_btn'
 end
 
 
